b9574bc5b10ab2c03882e721c24f7af755f5a1b3
Uzairrr1 <mohd.uzair@knoldus.com>
2022-12-19 16:26:24 +0530

    initial commit

---
+++ src/main/java/com/crud/example/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.crud.example.repository;
+
+import com.crud.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
